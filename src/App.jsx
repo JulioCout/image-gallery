bf4e5123666b69798ec41a9f207d435e4c59b257
Julio Coutinho <juliosilvacoutinho@gmail.com>
2022-10-02 19:08:23 -0300

    Images on start finalization

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import "./styles/global.scss"
 import './App.scss';
@@ -8,32 +8,38 @@ export default function App() {
   const [images, setImages] = useState([])
   const [inputValue, setInputValue] = useState([])
   const [page, setPage] = useState(2)
+  const [addButtonClass, setAddButtonClass] = useState("hide")
 
-
+    useEffect(() => {
+      const fetchImages = async () => {
+        const response = await fetch(
+          `https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`)
+        const data = await response.json()
+        setImages(data)
+      }
+      fetchImages()
+    }, [])
 
     function handleImageSearch(){
-      const fetchImages = async () => {
+      const  fetchImages =  async () => {
         const response = await fetch(`https://api.unsplash.com/search/photos/?query=${inputValue}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`)
         const data = await response.json()
         setImages(data.results)
         console.log(images)
         setPage(2)
+        setAddButtonClass("")
       }
-      
       fetchImages()
     }
 
     function handleAddImages(){
       const fetchImagesToAdd = async () => {
-        let response2 = await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${inputValue}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`)
-        let data2 = await response2.json()
-
-        console.log(data2.results)
-
-        setImages(prevState => [...prevState, ...data2.results])
+        const response = await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${inputValue}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`)
+        const data = await response.json()
+        setImages(prevState => [...prevState, ...data.results])
+        console.log(images)
         setPage(page + 1)
       }
-      
       fetchImagesToAdd()
     }
 
@@ -61,8 +67,12 @@ export default function App() {
         ))}
       </div>
 
+      <div className="addButton-section">
+        <button className={addButtonClass} onClick={handleAddImages}>Mostrar Mais</button>
+      </div>
+
       <footer>
-        <button className={images.length === 0 ? "hide" : "none"} onClick={handleAddImages}>Mostrar Mais</button>
+        <p>Feito por <a href="https://www.juliocoutinho.dev" target="_blank">Julio Coutinho</a></p>
       </footer>
 
     </div>
